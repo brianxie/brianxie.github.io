d32c72f7f4e5536a8542f15cb3daa989c55d67ff
Brian Xie <briancxie@gmail.com>
2020-12-27 14:43:25 -0800

    Update js files to tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
-function Website({ Component, pageProps }) {
+function Website({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
